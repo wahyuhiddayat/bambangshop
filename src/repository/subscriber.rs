35e04a7a13e8aae6f69d5a724484fdeb1f990676
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-04-16 10:39:55 +0700

    Implement list_all function in Subscriber repository.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -12,11 +12,21 @@ pub struct SubscriberRepository;
 impl SubscriberRepository {
     pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
         let subscriber_value = subscriber.clone();
-        if SUBSCRIBERS.get(product_type).is_none() {
-            SUBSCRIBERS.insert(String::from(product_type), DashMap::new())
+        if SUBSCRIBERS.get(product_type).is_none(){
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
         };
 
-        SUBSCRIBERS.get(product_type).unwrap().insert(subscriber_value.url.clone(), subscriber_value);
+        SUBSCRIBERS.get(product_type).unwrap()
+            .insert(subscriber_value.url.clone(), subscriber_value);
         return subscriber;
+    }
+
+    pub fn list_all(product_type: &str) -> Vec<Subscriber> {
+        if SUBSCRIBERS.get(product_type).is_none(){
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        };
+
+        return SUBSCRIBERS.get(product_type).unwrap().iter()
+            .map(|f| f.value().clone()).collect();
     }
 }
